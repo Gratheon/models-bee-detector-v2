d60923fa55c5dbb644adeeb55d4be10156334b79
Artjom Kurapov <artkurapov@gmail.com>
2024-09-23 00:21:30 +0300

    print results

--- count.py
+++ count.py
@@ -5,18 +5,23 @@ from ultralytics import YOLO, solutions
 model = YOLO("best.pt")
 cap = cv2.VideoCapture("314.mp4")
 assert cap.isOpened(), "Error reading video file"
-w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
+w, h, fps = (
+    int(cap.get(x))
+    for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS)
+)
 
-# Define line points
-line_points = [(20, 400), (1080, 400)]
+# Define region points
+region_points = [(20, 300), (800, 300)]
 
 # Video writer
-video_writer = cv2.VideoWriter("object_counting_output.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+video_writer = cv2.VideoWriter(
+    "object_counting_output.mp4", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h)
+)
 
 # Init Object Counter
 counter = solutions.ObjectCounter(
     view_img=True,
-    reg_pts=line_points,
+    reg_pts=region_points,
     names=model.names,
     draw_tracks=True,
     line_thickness=2,
@@ -25,7 +30,9 @@ counter = solutions.ObjectCounter(
 while cap.isOpened():
     success, im0 = cap.read()
     if not success:
-        print("Video frame is empty or video processing has been successfully completed.")
+        print(
+            "Video frame is empty or video processing has been successfully completed."
+        )
         break
     tracks = model.track(im0, persist=True, show=False)
 
@@ -35,3 +42,6 @@ while cap.isOpened():
 cap.release()
 video_writer.release()
 cv2.destroyAllWindows()
+
+print("counter: ", counter.in_counts)
+print("counter: ", counter.out_counts)
